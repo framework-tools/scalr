4376d21b9c8b4430369c4b89cdbebdc8c2381883
DominusVilicus <albertmashy@outlook.com>
2020-10-20 18:03:38 +1030

    start master

--- cluster/index.js
+++ cluster/index.js
@@ -1,5 +1,3 @@
-console.log('hello')
-
 import Koa from 'koa'
 import etcd from 'etcd3';
 import axios from 'axios';
@@ -16,58 +14,59 @@ let app = new Koa()
 // setup koa app that sets up API
 // use etcd for APi
 
-let info = {
-    pods: [
-        { 
-            ip: '127.0.0.1',
-            namespace: 'live',
-            service: 'frontend',
-            deployment: '2',
-            node: '127.0.0.1'
-        }
-    ],
-    nodes: [
-        {
-            ip: '127.0.0.1',
-            status: 'running'
-        }
-    ],
-    namespace: [
-        {
-            name: 'live',
+// let info = {
+//     pods: [
+//         { 
+//             ip: '127.0.0.1',
+//             namespace: 'live',
+//             service: 'frontend',
+//             deployment: '2',
+//             node: '127.0.0.1'
+//         }
+//     ],
+//     nodes: [
+//         {
+//             ip: '127.0.0.1',
+//             status: 'running'
+//         }
+//     ],
+//     namespace: [
+//         {
+//             name: 'live',
 
-        }
-    ],
-    services: [
-        {
-            name: 'frontend',
+//         }
+//     ],
+//     services: [
+//         {
+//             name: 'frontend',
 
-        }
-    ]
-}
+//         }
+//     ]
+// }
 
 // TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` \
 // && curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/
 
-async function getEC2AccessToken() {
-    console.log('Printing token:')
-    console.log(await axios({
-        method: 'get',
-        url: 'http://169.254.169.254/latest/meta-data/iam/security-credentials/cluster-iam',
-        // headers: {
-        //     'X-aws-ec2-metadata-token-ttl-seconds': '21600'
-        // },
-        // data: ''
-    }))
-    let { data } = await axios.put('http://169.254.169.254/latest/api/token', null,  {
-        headers: {
-            'X-aws-ec2-metadata-token-ttl-seconds': '21600'
-        }
-    })
+// async function getEC2AccessToken() {
+//     console.log('Printing token:')
+//     console.log(await axios({
+//         method: 'get',
+//         url: 'http://169.254.169.254/latest/meta-data/iam/security-credentials/cluster-iam',
+//         // headers: {
+//         //     'X-aws-ec2-metadata-token-ttl-seconds': '21600'
+//         // },
+//         // data: ''
+//     }))
+//     let { data } = await axios.put('http://169.254.169.254/latest/api/token', null,  {
+//         headers: {
+//             'X-aws-ec2-metadata-token-ttl-seconds': '21600'
+//         }
+//     })
 
-    console.log(data)
+//     console.log(data)
     
-    return data
-}
+//     return data
+// // }
 
-console.log(await getEC2AccessToken())
+// console.log(await getEC2AccessToken())
+
