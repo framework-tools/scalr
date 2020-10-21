e2ffa820c9042583050ee6c3fa138fdeae2b52a1
DominusVilicus <albertmashy@outlook.com>
2020-10-21 16:08:07 +1030

    Pus

--- master/index.js
+++ master/index.js
@@ -1,1 +1,29 @@
+import { exec } from 'child_process'
 
+/**
+ * Executes a shell command and return it as a Promise.
+ * @param command {string}
+ * @return {Promise<string>}
+ */
+function cmd (command) {
+    return new Promise((resolve, reject) => {
+        exec(command, (err, stdout, stderr) => {
+            if (err) console.error(err)
+            stdout ? resolve(stdout) : reject(stderr)
+        })
+    })
+}
+
+console.log(await cmd('dockerd'))
+
+// #!/bin/zsh
+// dockerd & echo "Starting docker daemon..."
+
+// until docker version > /dev/null 2>&1
+// do
+//     sleep 1;
+//     echo '.';
+// done
+
+// echo 'Started docker daemon'
+// #service docker start && \\
